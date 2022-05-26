c45adcd0ca751e654d2bd1973d9474e6d6393c40
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-05-26 23:17:02 +0300

    Create script.js

---
+++ js_lesson_3/script.js
@@ -0,0 +1,33 @@
+//основные операторы в js
+5*7//оператор умножения с левым и правым операндами 
+var x = 1;
+x=-x;//'-' унарный опреатор 
+alert(x);
+
+var num_1=1, num_2=3;
+alert(num_1-num_2);
+
+var str_1="Hello "+"World!";
+alert(str_1);
+
+alert('5'+2);//52
+alert('5'-2);//3
+alert(5/2);//2.5
+
+x=2*2+1;
+alert(x);
+
+var a,b,c;
+a=b=c=7;
+alert(a);
+alert(b);
+alert(c);
+
+alert(8%3);//2
+
+var i=5;
+alert(i++);//6
+
+var n=5;
+n*=2;//10
+
