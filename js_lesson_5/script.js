061531d43f6bc590086bcc45ca877e998327ebee
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-05-27 22:36:30 +0300

    Update script.js

--- js_lesson_5/script.js
+++ js_lesson_5/script.js
@@ -15,3 +15,24 @@ alert(result_1);
 var login=prompt('Введите login:','');
 var message=(login=='Сотрудник')?'Привет':(login=='Директор')? 'Здравствуйте':(login=='')?'Нет логина':'';
 alert(message);
+
+
+var login_2=prompt("Введите login:",'');
+if(login_2=='Админ'){
+    var password=prompt('Введите password','');
+    if(password=='Я главный'){
+        alert('Здравствуйте!')
+    }
+    else if(password==''||password==null){
+        alert('Отменено');
+    }
+    else{
+        alert('Неверный пароль');
+    }
+}
+else if(login_2==''|| login_2==null){
+    alert("Отменено");
+}
+else{
+    alert("Я вас не знаю")
+}
