4adc190b9a7db2f139506ef9731d133316ed4df3
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-05-27 21:32:50 +0300

    Create script.js

---
+++ js_lesson_4/script.js
@@ -0,0 +1,9 @@
+//Взаимодействие с пользователем: alert,promt, confirm
+alert('Сообщение')
+var result=prompt('Сколько тебе лет?',20);
+alert(`Тебе - ${result}`);
+var user=prompt('Как тебя зовут?',' ');
+alert(`Привет, ${user}`);
+var result_2=confirm("Ты согласен?");
+alert(result_2);
+
